f5d6989eafb4f9e353e28cdfad50a1298ac0d26d
masseedeh.assida <masseedeh.assida@xngage.com>
2016-09-14 12:06:55 +0300

    New initial project

---
+++ ShiraApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShiraApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShiraEntities db = new ShiraEntities();
+        public ActionResult Index()
+        {
+            var x = db.LanguageDictionary.Count();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ShiraApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShiraApplication.MvcApplication" Language="C#" %>
